3480fecb856722f9e164edb43214560ee84aaabb
Paul <paul.dessain@gmail.com>
2025-06-28 14:52:32 +0200

    [+] Finish v0 of GPX_Constructor

--- create_xml.py
+++ create_xml.py
@@ -8,7 +8,7 @@ class GPX_Constructor:
         self.time = time
         self.data = data
 
-    def init_gpx(self):
+    def compose_gpx(self):
         self.gpx = ET.Element(
             "gpx",
             xmlns_xsi="http://www.w3.org/2001/XMLSchema-instance",
@@ -24,34 +24,39 @@ class GPX_Constructor:
             xmlns_gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1",
             xmlns_gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3",
         )
+        # Metadata
         metadata = ET.SubElement(self.gpx, "metadata")
         ET.SubElement(metadata, "time").text = self.time  # TODO: reformatting time
+        trk = ET.SubElement(self.gpx, "trk")
+        ET.SubElement(trk, "name").text = self.name
+        ET.SubElement(trk, "type").text = self.type
+        trkseg = ET.SubElement(trk, "trkseg")
+        for idx, row in self.data.iterrows():
+            trkpt = ET.SubElement(
+                trkseg, "trkpt", lat=str(row["lat"]), lon=str(row["lon"])
+            )
+            ET.SubElement(trkpt, "ele").text = str(row["ele"])
+            ET.SubElement(trkpt, "time").text = row["time"]
+            # Extensions
+            if len(self.data.columns.isin(["hr", "cad"])) > 1:
+                extensions = ET.SubElement(trkpt, "extensions")
+                track_point_extension = ET.SubElement(
+                    extensions,
+                    "gpxtpx:TrackPointExtension",
+                    xmlns_gpxtpx="http://www.garmin.com/"
+                    "xmlschemas/TrackPointExtension/v1",
+                )
+                if len(self.data.columns.isin(["hr"])) > 1:
+                    ET.SubElement(track_point_extension, "gpxtpx:hr").text = str(
+                        row["hr"]
+                    )
+                if len(self.data.columns.isin(["cad"])) > 1:
+                    ET.SubElement(track_point_extension, "gpxtpx:cad").text = str(
+                        row["cad"]
+                    )
 
-
-# TODO : Finish construction
-# # Ajouter le segment de la track
-# trk = ET.SubElement(gpx, 'trk')
-# ET.SubElement(trk, 'name').text = '5kms samedi matin'
-# ET.SubElement(trk, 'type').text = 'running'
-
-# # Ajouter le trkseg (segment de track)
-# trkseg = ET.SubElement(trk, 'trkseg')
-
-# # Ajouter chaque point de track (trkpt) à partir de la DataFrame
-# for idx, row in df.iterrows():
-#     trkpt = ET.SubElement(trkseg, 'trkpt', lat=str(row['lat']), lon=str(row['lon']))
-#     ET.SubElement(trkpt, 'ele').text = str(row['ele'])
-#     ET.SubElement(trkpt, 'time').text = row['time']
-
-#     # Ajouter les extensions pour chaque point de track
-#     extensions = ET.SubElement(trkpt, 'extensions')
-#     track_point_extension = ET.SubElement(extensions, 'gpxtpx:TrackPointExtension',
-#     xmlns_gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1")
-#     ET.SubElement(track_point_extension, 'gpxtpx:hr').text = str(row['hr'])
-#     ET.SubElement(track_point_extension, 'gpxtpx:cad').text = str(row['cad'])
-
-# # Créer l'arbre XML et écrire dans un fichier
-# tree = ET.ElementTree(gpx)
-# tree.write('output.gpx', encoding='UTF-8', xml_declaration=True)
-
-# print("Le fichier XML a été généré avec succès.")
+    def generate_gpx(self):
+        self.compose_gpx()
+        tree = ET.ElementTree(self.gpx)
+        tree.write("output.gpx", encoding="UTF-8", xml_declaration=True)
+        print("Le fichier XML a été généré avec succès.")
